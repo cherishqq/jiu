4d05456cb38658ce84d079d883983d41c9e4ac5b
derek <haijinme@qq.com>
2013-05-06 22:30:05 +0800

    add help

---
+++ huizhan_hotel/help.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css">
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+<body>
+  
+  	<div data-role="content" class="jqm-content">
+		<h1>帮助</h1>
+
+		<p class="jqm-intro">This section explores how you can customize, extend, re-style and take full advantage of jQuery Mobile.</p>
+
+		<ul data-role="listview" data-inset="true" data-filter="true" data-theme="d" data-divider-theme="d" data-icon="false" data-filter-placeholder="Search demos..." data-global-nav="demos" class="jqm-list">
+        	<li data-role="list-divider">在线客服</li>
+			<li data-section="Demo Showcase" data-filtertext="dynamic collapsible set accordion append expand">
+				<a href="examples/collapsibles/dynamic-collapsible.php" data-ajax="false">酒店预订</a>
+			</li>
+			<li data-section="Demo Showcase" data-filtertext="dynamic collapsible set accordion append expand">
+				<a href="examples/collapsibles/dynamic-collapsible.php" data-ajax="false">酒店主页</a>
+			</li>
+        	<li data-section="Demo Showcase" data-filtertext="dynamic collapsible set accordion append expand">
+				<a href="examples/collapsibles/dynamic-collapsible.php" data-ajax="false">优惠信息查询</a>
+			</li>
+        	
+		</ul>
+
+	</div>
+</body>
+</html>
+
