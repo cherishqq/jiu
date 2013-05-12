35caa4ec263c3bb9caaac9c536ef9efdf070363d
derek.pan <haijinme@qq.com>
2013-05-12 21:53:29 +0800

    add 抽奖

---
+++ test2.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html >
+<html>
+<head>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,height=device-height,inital-scale=1.0,maximum-scale=1.0,user-scalable=no;" />
+<title>酒店预订</title>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css"/>
+<script  src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script type="javascript" src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script> -->
+
+<script  >
+     function init(){
+		alert("heh");
+	 }
+
+	 
+	
+	 /*
+    for( a in window ){
+		alert(a);
+		}
+	}
+	*/
+   
+   
+</script>
+</head>
+<body onload="init()">
+    <div id="test">	测试
+</div>
+	
+</body>
+</html>
